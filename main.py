4a14007982791c7e3c9d28c8d4730a68d41433b5
filipe11402 <filipe11402@outlook.pt>
2021-04-07 11:38:43 +0100

    updated code with comments

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ def get_data():
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    results = soup.find_all("div", class_="maincounter-number")
+    results = soup.find_all("div", class_="maincounter-number")  #returns a list
 
+
+    # getting data from each index with span tag
     total_cases = results[0].find("span")
     total_deaths = results[1].find("span")
     recovered_cases = results[2].find("span")
+
+    # printing the numbers we wanted
     print(total_cases.text)
     print(total_deaths.text)
     print(recovered_cases.text)
